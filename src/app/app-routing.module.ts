e3f1646c7ac28755a13cb097a5c059ef1dc42f26
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-12-11 17:28:18 +0100

    aggiunta btn per le routers e logica di routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path:'home',
+  //   loadChildren:()=> import('./competenze/competenze.module').then(c => c.CompetenzeModule),
+  // },
+  // {
+  //   path:'settings',
+  //   loadChildren:()=> import('./dipendenti/dipendenti.module').then(d => d.DipendentiModule),
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+//import { RouterModule } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
+    // RouterModule.forRoot([
+    //   { path: '', pathMatch: 'full', redirectTo: 'competenze' },
+    //   { path: 'competenze', component: CompetenzeComponent },
+    // ])
   ],
   providers: [],
   bootstrap: [AppComponent]
