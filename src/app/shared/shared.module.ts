2432f6ca389a02a5f82c32d68f95a780cfac477a
Francesco Iavazzo <f.iavazzo@ecample.com>
2023-12-12 16:11:58 +0100

    fix

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import {MatToolbarModule} from '@angular/material/toolbar';
-<<<<<<< HEAD
 import {MatTabsModule} from '@angular/material/tabs';
-import { MatTableModule } from '@angular/material/table';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
 
-=======
-import { MatTabsModule } from '@angular/material/tabs';
->>>>>>> dbdb129b546b1f09f91021f43dbee2d31ac82a35
 @NgModule({
   declarations: [],
   imports: [
