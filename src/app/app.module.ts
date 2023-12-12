cd1393a9203c92eaae3735b626d5ab4ab00d34c9
Francesco Vezzuto <f.vezzuto@example.com>
2023-12-12 15:44:07 +0100

    change path color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-=======
 import { NavbarComponent } from './navbar/navbar.component';
-// import { RouterModule } from '@angular/router';
-// import { CompetenzeComponent } from './competenze/competenze.component';
-import { SharedModule } from './shared/shared.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
->>>>>>> dbdb129b546b1f09f91021f43dbee2d31ac82a35
 
 @NgModule({
   declarations: [
@@ -30,10 +24,6 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-<<<<<<< HEAD
-=======
-    SharedModule,
->>>>>>> dbdb129b546b1f09f91021f43dbee2d31ac82a35
   ],
   providers: [],
   bootstrap: [AppComponent]
