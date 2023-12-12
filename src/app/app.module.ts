b7656ef8da911d53be21e07ad78fd55866ee6a04
Francesco Vezzuto <f.vezzuto@example.com>
2023-12-12 10:33:26 +0100

    modifiche fatte da vittoria da fixare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CompetenzeComponent } from './competenze/competenze.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CompetenzeComponent } from './competenze/competenze.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
