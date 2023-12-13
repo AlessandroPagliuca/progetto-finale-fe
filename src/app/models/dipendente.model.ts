59b7e4e078b252a16d9b98e828951c47ca1d08cc
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-12-13 12:48:52 +0100

    init service dipendente and environment url

--- src/app/models/dipendente.model.ts
+++ src/app/models/dipendente.model.ts
@@ -1,3 +1,3 @@
 export class Dipendente {
-
+    id?: number | string;
 }
--- src/app/services/dipendente.service.ts
+++ src/app/services/dipendente.service.ts
@@ -1,10 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Dipendente } from '../models/dipendente.model';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class DipendenteService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  findAllDipendenti(): Observable<Dipendente[]> {
+    return this.http.get<Dipendente[]>(environment.url + '/dipendenti');
+  }
+  updateDipendente(dipendente: Dipendente): Observable<Dipendente> {
+    let body = dipendente;
+    let id = dipendente.id;
+    return this.http.put<Dipendente>(environment.url + `/dipendenti/${id}`, body);
+  }
+  getByIdDipendente(id: number | string): Observable<Dipendente> {
+    //console.log('ID:', id);
+    return this.http.get<Dipendente>(environment.url + `/detail/${id}`);
+  }
 }
